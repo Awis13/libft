514c0f4b32d95ce7c67bdfc6292b66a61eeedc32
Awis13 <awis@me.com>
2021-11-15 14:57:09 +0000

    ft_isascii

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: Nipostni <awis@me.com>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 13:50:15 by Nipostni          #+#    #+#             */
-/*   Updated: 2021/11/15 14:50:38 by Nipostni         ###   ########.fr       */
+/*   Updated: 2021/11/15 14:56:38 by Nipostni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
 int	ft_isalpha(int argument);
 int	ft_isdigit(int argument);
 int	ft_isalnum(int argument);
+int	ft_isascii(int argument);
 int	test_ft_isdigit(void);
 int	test_ft_isalpha(void);
 int	test_ft_isalnum(void);
+int	test_ft_isascii(void);
 
 int	main(void)
 {
@@ -26,6 +28,8 @@ int	main(void)
 	test_ft_isdigit();
 	printf("\n");
 	test_ft_isalnum();
+	printf("\n");
+	test_ft_isascii();
 	printf("\n");
 }
 
@@ -76,4 +80,20 @@ int	test_ft_isalnum(void)
 	printf("%d, testing %c \n", ft_isalnum(to_pass), to_pass);
 	to_pass = '*';
 	printf("%d, testing %c \n", ft_isalnum(to_pass), to_pass);
+}
+
+/* Testing ft_isalnum */
+int	test_ft_isascii(void)
+{
+	int	to_pass;
+
+	to_pass = '_';
+	printf("*** ft_isdigit ***\n");
+	printf("%d, testing %c \n", ft_isascii(to_pass), to_pass);
+	to_pass = '1';
+	printf("%d, testing %c \n", ft_isascii(to_pass), to_pass);
+	to_pass = 'A';
+	printf("%d, testing %c \n", ft_isascii(to_pass), to_pass);
+	to_pass = '*';
+	printf("%d, testing %c \n", ft_isascii(to_pass), to_pass);
 }

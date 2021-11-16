77e1c7630f633b3a4c474a28e101ca91f4cd1fa8
Awis13 <awis@me.com>
2021-11-16 15:52:36 +0000

    bzero_b

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,25 @@
 /*   By: Nipostni <awis@me.com>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 14:58:42 by Nipostni          #+#    #+#             */
-/*   Updated: 2021/11/16 14:59:58 by Nipostni         ###   ########.fr       */
+/*   Updated: 2021/11/16 15:52:15 by Nipostni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+void *ft_memset(void *s, int c, size_t n);
+void ft_bzero (void *b, size_t length)
+{
+	ft_memset(b, 0, length * sizeof(char));
+}
+
+int	main(void)
+{
+	char str[20] = "1234567890";
+	ft_bzero(str, 4);
+	printf("%s\n", str);
+	printf("%d\n", str[1]);
+
+}

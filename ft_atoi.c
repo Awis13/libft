95ee2608afc7f6548fcc0d09e39209ec5c42972b
Awis13 <awis@me.com>
2021-12-03 22:03:42 +0000

    ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -17,34 +17,27 @@
 int ft_atoi(const char *nptr)
 {
 	int i;
-	int number;
-	int len;
+	long number;
 	char sign;
 
 	number = 0;
-	len = ft_strlen(nptr);
 	i = 0;
 	sign = 1;
 	
-	while (nptr[i] == ' ' || nptr[i] == '\t' || nptr[i] == '\n' || nptr[i] == '\f' || nptr[i] == '\r')
-		i++;
-		
-	while (nptr[i] >= '0' && nptr[i] <= '9')
+	while (nptr[i] == ' ' || nptr[i] == '\t' || nptr[i] == '\n' || nptr[i] == '\f' || nptr[i] == '\r' || nptr[i] == '\v')
+			i++;
+
+	if ((nptr[i] == '+') || (nptr[i] == '-'))
 	{
-		if (nptr[i] + '0' == 0)
-		{
-			i++;
-		}
-		if (nptr[i] == '+')
-		{
-			i++;
-		}
-		if (nptr[i] == '-')
+		if(nptr[i] == '-')
 		{
 			sign = -sign;
-			i++;
 		}
-		number = number * 10 + nptr[i] - '0';
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		number = number * 10 + (nptr[i] - '0');
 		i++;
 	}
 
@@ -54,7 +47,7 @@ int ft_atoi(const char *nptr)
 
 // int main(void)
 // {
-// 	char c[20] = " 01545";
-// 	printf("%d ", ft_atoi(c));
-// 	printf("%d ", atoi(c));
+// 	char c[30] = "469";
+// 	printf("%d My Atoi \n", ft_atoi(c));
+// 	printf("%d System Atoi \n", atoi(c));
 // }
